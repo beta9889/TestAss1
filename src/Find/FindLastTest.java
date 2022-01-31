df784ef59957fc5de88d4686272c01a9ccb3b356
EricKR <78589314+EricKRPe@users.noreply.github.com>
2022-01-31 19:35:27 +0100

    Test

--- src/Find/FindLastTest.java
+++ src/Find/FindLastTest.java
@@ -9,7 +9,7 @@ public class FindLastTest
     // this test fails!
     @Test public void lastOccurrenceInFirstElement()
     {
-        int arr[] = {2, 3,5};
+        int[] arr = {2, 3,5};
         int y = 2;
         assertEquals("Last occurrence in first element", 0, FindLast.findLast(arr, y));
     }
@@ -22,7 +22,7 @@ public class FindLastTest
     @Test
     //2 not first position => not fucked
     public void lastOccNoFault(){
-        int arr[] = {0,1,2,3,4};
+        int[] arr = {0,1,2,3,4};
         int y = 2;
         assertEquals("not first position still error", 2,FindLast.findLast(arr,y));
 
@@ -31,7 +31,7 @@ public class FindLastTest
     @Test
     //3 still skips first position however expected is -1 so no error
     public void lastOccNotInArray(){
-        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int[] arr = {1,2,3,4,5,6,7,8,9};
         int y = 0;
         assertEquals("Something else fucked?", -1,FindLast.findLast(arr,y));
     }
@@ -39,14 +39,14 @@ public class FindLastTest
     @Test
     //4 typo in test => error however program still does as expected.
     public void errorNoFailure(){
-        int arr[] = {1,2,3,4,5,6};
+        int[] arr = {1,2,3,4,5,6};
         int y = 2;
         assertEquals("Found correctly or something?", 2, FindLast.findLast(arr,y));
     }
 
     @Test public void lastOccurrenceInFirstElementFixed()
     {
-        int arr[] = {2, 3,5};
+        int[] arr = {2, 3,5};
         int y = 2;
         assertEquals("Last occurence in first element", 0, FindLast.findLastfixed(arr, y));
     }
@@ -55,7 +55,7 @@ public class FindLastTest
     @Test
     //2 not first position => not fucked
     public void lastOccNoFaultFixed(){
-        int arr[] = {0,1,2,3,4};
+        int[] arr = {0,1,2,3,4};
         int y = 2;
         assertEquals("not first position still error", 2,FindLast.findLastfixed(arr,y));
 
@@ -64,7 +64,7 @@ public class FindLastTest
     @Test
     //3 still skips first position however expected is -1 so no error
     public void lastOccNotInArrayFixed(){
-        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int[] arr = {1,2,3,4,5,6,7,8,9};
         int y = 0;
         assertEquals("Something else fucked?", -1,FindLast.findLastfixed(arr,y));
     }
